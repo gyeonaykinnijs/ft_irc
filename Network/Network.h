8d5778937ba579a04fac737175e55e1489bd9a8d
Gyujun Yeon <gyeon@c6r8s4.42seoul.kr>
2022-07-25 15:25:30 +0900

    style :컨벤션 준수

--- Network/Network.h
+++ Network/Network.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <vector>
+#include <utility>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -14,29 +16,31 @@ struct commandReq
 {
 	std::string prefix;
 	std::string command;
-	vector<std::string> parameters;
+	std::vector<std::string> parameters;
 	std::string parameterLast;
 };
 
 class Network
 {
 public:
-	Network(const std::string ip, const short port, const std::string passWord);
+	Network(const std::string ip, const short port, const std::string password);
 	~Network(); //RAII
-	Init();
-	bool SendToUser(const std::string& message, User& user);
-	bool SendToChannel(const std::string& message, Channel& channel);
-	bool Kick(const std::string& message, User& user);
+	void init();
+	bool IOMultiflexing();
+	bool sendToUser(User& user, const std::string& message);
+	bool sendToChannel(Channel& channel, const std::string& message);
+	bool kick(const std::string& message, User& user);
 	commandReq getCommand();
 private:
 	Network(const Network& other);
 	Network& operator=(const Network& other);
 	const std::string PASSWORD_;
 	sockaddr_in addressServer_;
-	const std::string IP;
+	const std::string IP_;
 	const short PORT_;
 	int fdServer_;
 	std::queue<commandReq> commandQueue_;
+	std::queue< std::pair<std::string, std::string> > sendQueue_;
 };
 
 #endif
