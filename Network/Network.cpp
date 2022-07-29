08312cceebc1ae503e173f1ef1ffef1898c6cc7e
Gyujun Yeon <gyeon@c5r2s4.42seoul.kr>
2022-07-29 10:23:41 +0900

    feat : 커맨드 스플릿 구현중

--- Network/Network.cpp
+++ Network/Network.cpp
@@ -14,6 +14,7 @@
 #include <cerrno>
 
 using namespace std;
+#define BUFFERSIZE 512
 
 Network::Network(const char* ip, const short port, const char* passWord, UserManager& userManager)
 : IP(ip), PORT(port), PASSWORD(passWord), userManager(userManager)
@@ -56,6 +57,7 @@ void Network::init()
 
 bool Network::IOMultiflexing()
 {
+	
 	// bind와 구분하기 위해, 아래의 bind는 namespace가 없다는걸 명시하기 위해 앞에 ::를 붙인다.
 	if (::bind(this->fdServer, reinterpret_cast<sockaddr*>(&this->addressServer), sizeof(this->addressServer)) < 0)
 	{
@@ -70,27 +72,76 @@ bool Network::IOMultiflexing()
 	while (1)
 	{
 		initFdSets();
-		if (::select(this->fdServer, &this->rSet, &this->wSet, NULL, NULL) < 0)
+		sleep(3);
+		// FIXME: 클라 끊겼을떄 정리하는 코드 작성
+		if (::select(10, &this->rSet, &this->wSet, NULL, NULL) < 0)
 		{
 			// FIXME: 수정 필요.
 			cerr << "[select]" << strerror(errno) <<endl;
 		}
+		//std::cout << "ssdddd" << std::endl;
 		if (FD_ISSET(this->fdServer, &this->rSet))
 		{
-			//int fdClient = ::accept(this->fdServer, )
+			sockaddr_in addressClient;
+			socklen_t lenClient = sizeof(addressClient);
+			int fdClient = ::accept(this->fdServer, reinterpret_cast<sockaddr*>(&addressClient), &lenClient);
+			if (fdClient < 0)
+			{
+				std::cerr << "[select]" << strerror(errno) <<endl;
+			}
+			this->userManager.makeUser(fdClient);
+			std::cout << fdClient << " user set" << std::endl;
 		}
 		else
 		{
-			// FIXME: ref로 변경 필요
-			map<int, User*> users = this->userManager.getAllUser();
+			map<int, User*>& users = this->userManager.getAllUser();
 			// 이미 연결된 유저들과 관련된 동작
 			for(map<int, User*>::iterator iter = users.begin(); iter != users.end(); iter++)
 			{
-				// FIXME: 추후 user이터레이터 얻고 나서 수정.
 				if (FD_ISSET(iter->first, &this->rSet))
 				{
-					// FIXME: 유저 생성시 fd값을 받게.
-
+					
+					int lenRecv;
+					char buffer[BUFFERSIZE];
+					lenRecv = ::recv(iter->first, buffer, BUFFERSIZE, 0);
+					//std::cout << "[" << iter->first << "] " << lenRecv << std::endl;
+					//write(1, buffer, lenRecv);
+					if (strnstr(buffer, "\r\n", lenRecv) == NULL)
+					{
+						
+						//TODO:512내에 CRLF가 안오면, 다음 CRLF까지 들어온 입력을 싹 날려주는 명령. 근데 User에서 가지고 있어야되서 일단 패스.
+						//int errorFlag = false; // 
+						// 유저단에서 버퍼도 가지고 있어야 할거 같은데? ㅋㅋㅋ -> 유저가 보낸 명령르 다 못받을 수도 있어서, 받을때까지 기다려야 하면, 이걸 일단 가지고 있어야한다.
+					}
+					while (1)
+					{
+						char* where = strnstr(buffer, "\r\n", lenRecv);
+						size_t len = where - buffer;
+						if (where == NULL)
+						{
+							//cout << lenRecv << endl;
+							//cout << where << endl;
+							printf("🌟%p\n", where);
+							break;
+							//TODO:no \r\n, 나올때까지 입력 날려버리기. -> buffer 꽉 차게 받받았았는데 CRLF가 없는 경우 CRLF나올때까지 모든 입력 무시.
+							// 2. 버퍼에 recv한 데이터가 짤려서 CRLF가 안들어간 경우 ->
+						}
+						else
+						{
+							string temp(buffer, 0, len);
+							cout << "🌟" << temp << "🌟" << endl;
+							if
+							{
+								lenRecv -= len + 2;
+								if (lenRecv == 0)
+								{
+									cout << "33333333333333" << endl;
+									break;
+								}
+								memcpy(buffer, buffer + len + 2, lenRecv);
+							}
+						}
+					}
 				}
 			}
 			// TODO: send작업은 좀 나중에 하기.
@@ -109,6 +160,8 @@ bool Network::IOMultiflexing()
 void Network::initFdSets()
 {
 	// rSet모든 유저 돌면서 set에 추가.
+	FD_ZERO(&this->rSet);
+	FD_ZERO(&this->wSet);
 	map<int, User*>::iterator iter =  this->userManager.getAllUser().begin();
 	map<int, User*>::iterator iterEnd =  this->userManager.getAllUser().end();
 	FD_SET(this->fdServer, &this->rSet);
